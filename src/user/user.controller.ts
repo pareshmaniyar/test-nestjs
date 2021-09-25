4741eeece221d40d65a4e41c090f4bb15070997a
pareshmaniyar <pareshmaniyar1@gmail.com>
2021-09-25 15:18:45 +0530

    Add User Service

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,16 +1,26 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { CreateUserDTO } from './dtos/create-user.dto';
+import { User } from './entities/user.entity';
 import { UserService } from './user.service';
 
+@ApiTags('users')
 @Controller('user')
 export class UserController {
   constructor(private userService: UserService) {}
 
   @Get()
-  getUsers(): any {
-    return [{ id: 0 }];
+  getUsers(): User[] {
+    return this.userService.findAll();
   }
+
   @Get(':id')
-  getUserByID(@Param('id') id: string): any {
-    return { id };
+  getUserByID(@Param('id') id: string): User {
+    return this.userService.findById(Number(id));
+  }
+
+  @Post()
+  createUser(@Body() body: CreateUserDTO): User {
+    return this.userService.createUser(body);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './entities/user.entity';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  private users: User[] = [
+    { id: 0, name: 'Paresh' },
+    { id: 1, name: 'Rakesh' },
+  ];
+
+  findAll() {
+    return this.users;
+  }
+  findById(id: number): User {
+    return this.users.find((user) => user.id === id);
+  }
+
+  createUser(CreateUserDTO): User {
+    this.users.push({ ...CreateUserDTO, id: this.users.length });
+    return this.users[this.users.length - 1];
+  }
+}
