8e13fbf32ccca5dec5b13222ae9f879704ced377
pareshmaniyar <pareshmaniyar1@gmail.com>
2021-09-26 15:27:36 +0530

    Add Swagger documentation

--- src/user/dtos/create-user.dto.ts
+++ src/user/dtos/create-user.dto.ts
@@ -1,3 +1,8 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateUserDTO {
+  @ApiProperty()
   name: string;
+  @ApiProperty({ required: false })
+  age: number;
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,24 +1,27 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { CreateUserDTO } from './dtos/create-user.dto';
 import { User } from './entities/user.entity';
 import { UserService } from './user.service';
 
 @ApiTags('users')
-@Controller('user')
+@Controller('users')
 export class UserController {
   constructor(private userService: UserService) {}
 
+  @ApiOkResponse({ type: User, isArray: true })
   @Get()
   getUsers(): User[] {
     return this.userService.findAll();
   }
 
+  @ApiOkResponse({ type: User })
   @Get(':id')
   getUserByID(@Param('id') id: string): User {
     return this.userService.findById(Number(id));
   }
 
+  @ApiCreatedResponse({ type: User })
   @Post()
   createUser(@Body() body: CreateUserDTO): User {
     return this.userService.createUser(body);
