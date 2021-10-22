cab3f6edc052acfc02156681b87c8155fe05e625
pareshmaniyar <pareshmaniyar1@gmail.com>
2021-10-22 20:33:07 +0530

    WIP: Add Student and teacher resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,10 @@ import { UserModule } from './user/user.module';
 import { TodosModule } from './todos/todos.module';
 import { ProductsModule } from './products/products.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { StudentsModule } from './students/students.module';
+import { TeachersModule } from './teachers/teachers.module';
+import { StudentModule } from './student/student.module';
+import { TeacherModule } from './teacher/teacher.module';
 import config from './ormconfig';
 
 @Module({
@@ -13,6 +17,10 @@ import config from './ormconfig';
     TodosModule,
     ProductsModule,
     TypeOrmModule.forRoot(config),
+    StudentsModule,
+    TeachersModule,
+    StudentModule,
+    TeacherModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/student/entities/student.entity.ts
@@ -0,0 +1,12 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+@Entity()
+export class Student {
+  @PrimaryGeneratedColumn()
+  id: number;
+  
+  @Column()
+  name: string;
+
+  @Column()
+  classId: User;
+}
